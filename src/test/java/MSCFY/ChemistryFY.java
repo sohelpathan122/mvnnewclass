5c7182cbf31dd92c6b746feb1ad3246accd94f1c
sohelpathan122 <sohelpathan5110@gmail.com>
2024-09-23 12:58:52 -0500

    Add Project

---
+++ src/test/java/MSCFY/ChemistryFY.java
@@ -0,0 +1,20 @@
+package MSCFY;
+
+import org.testng.annotations.Test;
+
+public class ChemistryFY {
+  @Test
+  public void False()
+  {
+	  int a=10;
+	  int b=20;
+	  System.out.println(a>b);
+  }
+  @Test
+  public void True()
+  {
+	  int a=10;
+	  int b=20;
+	  System.out.println(b>a);
+  }
+}
